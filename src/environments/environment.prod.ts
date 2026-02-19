61ad77a0e63842c7bb78539fbf9d05c4a0bc3a35
Hamza Mswi <hamza.missaoui47@gmail.com>
2026-02-19 14:11:46 +0100

    first commit for futur ai chatbot with angular

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  geminiApiKey: 'YOUR_PRODUCTION_GEMINI_API_KEY' // Replace with your production Gemini API key
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  geminiApiKey: 'AIzaSyB4K7q6Y8X9w2V3T5U6I7O8P9Q0R1S2T3U4' // Replace with your actual Gemini API key
-};
